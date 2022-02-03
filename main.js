f602f5560c1bfd63fffaac95f87f93274facc797
Azizah Elvia <azizahelviaa@gmail.com>
2022-02-04 00:22:53 +0700

    L8 - Computed Properties

--- main.js
+++ main.js
@@ -2,14 +2,21 @@ const app = Vue.createApp({
     data() { 
         return {
             cart: 0,
+            // Task!: tambahkan onSale bernilai true
+            onSale: true,
+            // End Task
             product: 'Kaus Kaki',
-            image: './assets/images/socks_green.jpg',
-            inventory: 0,
+            // Tambahkan brand
+            brand: 'Mayesz',
+            // ganti updateImage ke selectedVariant dgn nilai 0 tujuan
+            // untuk mengubah dgn index jenis warna yang dipilih
+            // dan hapus inventory
+            selectedVariant: 0,
             details: ['50% katun', '30% wol', '20% poliester'],
-            // dalam variants tambahkan url image
+            // dalam variants tambahkan inventory masing-masing index untuk jumlah barang yg tersedia
             variants: [
-                { id: 2110, color: 'green', image: './assets/images/socks_green.jpg'},
-                { id: 2111, color: 'blue', image: './assets/images/socks_blue.jpg'},
+                { id: 2110, color: 'green', image: './assets/images/socks_green.jpg', inventory: 0},
+                { id: 2111, color: 'blue', image: './assets/images/socks_blue.jpg', inventory: 50},
             ]
         }
     },
@@ -21,8 +28,29 @@ const app = Vue.createApp({
         },
         // Tambahkan method updateImage untuk melakukan panggilan
         // dari listener v-on:mouseover
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
         },
+    },
+    // Tambahkan object computed yg didalamnya berisi method yang mengembalikan
+    // nilai title gabungan antara brand dan product
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        // image digunakan untuk mengembalikan nilai varian gambar berdasarkan index
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        // inventory digunakan untuk mengembalikan nilai varian jumlah berdasarkan index
+        inventory() {
+            return this.variants[this.selectedVariant].inventory
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' sedang dijual!'
+            } 
+                return this.brand + ' ' + this.product + ' stok kosong!'
+        }
     }
 })
